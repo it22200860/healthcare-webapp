d13cf5b9c7033d3e93f972d45e9e438f05d12d07
it22200860 <128211334+it22200860@users.noreply.github.com>
2025-10-30 02:16:02 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Healthcare WebApp running via DevOps Pipeline 🚀"
+    return "Healthcare WebApp running via DevOps Pipeline"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
